7faa5419353e82d3778b56d8b595bade51f8cf4e
mleiria <manuel.leiria@gmail.com>
2017-01-27 14:12:40 +0000

    -

--- src/main/java/pt/mleiria/machinelearning/classification/Bayes.java
+++ src/main/java/pt/mleiria/machinelearning/classification/Bayes.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 
 import pt.mleiria.machinelearning.matrixAlgebra.Matrix;
-import pt.mleiria.numericalAnalysis.utils.ViewUtils;
+import pt.mleiria.machinelearning.statistics.ColumnStatistics;
 
 /**
  * @author manuel
@@ -104,27 +104,70 @@ public class Bayes {
 	 * @param dataSet
 	 * @return
 	 */
-	public Map<Integer, List<Double[]>> separateByClass(Matrix dataSet){
-		Map<Integer, List<Double[]>> separated = new HashMap<Integer, List<Double[]>>();
+	public Map<Integer, Matrix> separateByClass(Matrix dataSet){
+		final Map<Integer, List<Double[]>> separated = new HashMap<Integer, List<Double[]>>();
 		final int rows = dataSet.rows();
 		final int cols = dataSet.columns();
 		for(int i = 0; i < rows; i++){
 			final Integer key = (int) dataSet.component(i, cols - 1);
-			final Double[] data = new Double[cols - 1];
+			final Double[] data = new Double[cols];
 			List<Double[]> lst;
 			if(separated.containsKey(key)){
 				lst = separated.get(key);
 			}else{
 				lst = new ArrayList<Double[]>();
 			}
-			for(int j = 0; j < cols - 1; j++){
+			for(int j = 0; j < cols; j++){
 				data[j] = dataSet.component(i, j);
 			}
 			lst.add(data);
 			separated.put(key,lst);
 		}
-		log.info(ViewUtils.showSeparatedByClass(separated));
-		return separated;
+		final Map<Integer, Matrix> separatedMatrix = new  HashMap<Integer, Matrix>();
+		for (Map.Entry<Integer, List<Double[]>> entry : separated.entrySet()) {
+			separatedMatrix.put(entry.getKey(), convertToMatrix(entry.getValue()));
+		}
+		
+		return separatedMatrix;
+	}
+	
+	private Matrix convertToMatrix(List<Double[]> lst){
+		final int rows = lst.size();
+		final int cols = lst.get(0).length;
+		final double components[][] = new double[rows][cols];
+		for(int i = 0; i < rows; i++){
+			for(int j = 0; j < cols; j++){
+				components[i][j] = lst.get(i)[j];
+			}
+		}
+		return new Matrix(components);
+	}
+	/**
+	 * For each column in Matrix calculates avg and stdev.
+	 * The structure is already divided by class
+	 * 
+	 * @param map
+	 * @return a Map where the key is the class and the value is
+	 * a Matrix(2xn) where Matrix(0,i) is the avg of col i and Matrix(1, i)
+	 * is the stdev of col i
+	 */
+	public Map<Integer, Matrix> getAvgStdev(final Map<Integer, Matrix> map){
+		
+		Map<Integer, Matrix> summaryByClass = new HashMap<Integer, Matrix>();
+		for(final Map.Entry<Integer, Matrix> entry : map.entrySet()){
+			final List<Double[]> avgStdev = new ArrayList<Double[]>();
+			final Matrix m = entry.getValue();
+			double[][] components = new double[2][m.columns() - 1];
+			for(int i = 0; i < m.columns() - 1; i++){
+				final ColumnStatistics cs = new ColumnStatistics(m.getColumn(i));
+				final double avg = cs.getAverage();
+				final double stdev = cs.getStandardDeviation();
+				components[0][i] = avg;
+				components[1][i] = stdev;
+			}
+			summaryByClass.put(entry.getKey(), new Matrix(components));
+		}
+		return summaryByClass;
 	}
 
 	/**
@@ -133,7 +176,7 @@ public class Bayes {
 	public static void main(String[] args) {
 		Bayes b = new Bayes("/home/manuel/tools/adalineProcesses/mlearning/bayes/pima-indians-diabetes.data.csv", 0.67);
 		b.loadDataSet();
-		Map<Integer, List<Double[]>> tmp = b.separateByClass(b.dataSet);
+		Map<Integer, Matrix> tmp = b.separateByClass(b.dataSet);
 		
 	}
 
